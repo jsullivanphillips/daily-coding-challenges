97935dd92effad50b5fa2b04ea06a89082301b07
jsullivanphillips <jamiesullphill@gmail.com>
2022-04-20 09:49:22 -0700

    april 20th, product of elements except i

---
+++ april_20.rb
@@ -0,0 +1,10 @@
+# Jamie Sullivan-Phillips
+# Daily coding challenge April 20th 2022
+input = [3,2,1]
+
+def product_except_i(input)
+    for elements in input
+        puts(elements)
+end
+
+product_except_i(input)
