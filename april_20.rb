6ff210f0fc602d3494fe3b88da339f3248f81ce3
jsullivanphillips <jamiesullphill@gmail.com>
2022-04-20 16:30:08 -0700

    added multiply without i, with no div

--- april_20.rb
+++ april_20.rb
@@ -1,10 +1,26 @@
 # Jamie Sullivan-Phillips
 # Daily coding challenge April 20th 2022
-input = [3,2,1]
+input = [1,2,3,4,5]
 
 def product_except_i(input)
+    output = []
+    product = input.reduce(1, :*)
     for elements in input
-        puts(elements)
+        output.push(product / elements)
+    end
+    return output
 end
 
-product_except_i(input)
+def product_except_i_nodiv(input)
+    output = []
+    for elements in input
+        output.push(input.inject do |product, num|
+                num == elements ? product : product = product * num
+            end)
+    end
+    return output
+end
+
+
+puts(product_except_i(input))
+puts(product_except_i_nodiv(input))
