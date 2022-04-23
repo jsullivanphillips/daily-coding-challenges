ff52f6eeb4256c276c09329f57c49b8e936df751
jsullivanphillips <jamiesullphill@gmail.com>
2022-04-22 22:24:35 -0700

    added comments

--- april_22.rb
+++ april_22.rb
@@ -13,6 +13,10 @@
 # ------------------ #
 # Assumptions: its not about a pattern. its 
 
+
+# Organizer method. 
+# Calls segregate then builds shifted positive array.
+# Then calls findMissingPositive with shifted array.
 def findMissing(arr)
     size = arr.size
     shift = segregrate(arr, size)
@@ -28,6 +32,8 @@ def findMissing(arr)
     return findMissingPositive(pos_arr, j)
 end
 
+# Segregate moves all negative and zero elements to the left side
+# of array, and returns the index of the first positive element.
 def segregrate(arr, size)
     j = 0
     i = 0
@@ -43,6 +49,13 @@ def segregrate(arr, size)
     return j
 end
 
+# findMissingPositive traverses the elements of the all positive array.
+# if the element value is less then the length of the array, then set
+# value in the array at the index of the elements value (array[arr[i]])
+# to negative. 
+# After this is complete, traverse the array a second time and return
+# the index of the first positive value encountered. If no positive values
+# are encountered then reutnr the length of the array + 1.s
 def findMissingPositive(arr, size)
     i = 0
     while i < size 
